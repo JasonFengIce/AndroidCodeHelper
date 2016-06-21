0f890ea4e8b962f344058315564517d102d76e04
fenghuibin <fenghuibin@ismartv.cn>
2016-06-21 15:44:24 +0800

    init

--- DpiTransformer/ViewController.swift
+++ DpiTransformer/ViewController.swift
@@ -8,7 +8,11 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSXMLParserDelegate {
+    var mPathValue: String = ""
+
+    @IBOutlet weak var mPathTextField: NSTextField!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +22,81 @@ class ViewController: NSViewController {
 
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
+    }
+
+    @IBAction func onButtonClick(sender: NSButton) {
+        mPathValue = mPathTextField.stringValue
+
+        mPathValue = "/Users/huibin/AndroidProject/Daisy/Daisy/res/values-sw1080dp/dimens.xml";
+        checkPath(mPathValue)
+    }
+
+    func checkPath(path: String) {
+        var isDirectory: ObjCBool = false;
+        let fileManger = NSFileManager.defaultManager();
+        let exist: Bool = fileManger.fileExistsAtPath(path, isDirectory: &isDirectory);
+
+
+        if exist {
+            if isDirectory {
+                print("\"\(path)\" is directory")
+            } else {
+                parseXml(path)
+            }
+
+        } else {
+            print("\"\(path)\" is not exist")
+        }
+
+    }
+
+
+    func showDialog() {
+
+    }
+
+    func parseXml(path: String) {
+        let url = NSURL(fileURLWithPath: path)
+
+        let xmlParser = NSXMLParser(contentsOfURL: url)
+
+        xmlParser!.delegate = self;
+        xmlParser!.parse();
+
+    }
+
+    func parseXmlFromString(string: String) {
+
+//        let data = NSData(string, data)
+//        let xmlParser = NSXMLParser();
+//
+//        xmlParser!.delegate = self;
+//        xmlParser!.parse();
+    }
+
+
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String:String]) {
+        print("start element: \(elementName)")
+        print("\(attributeDict)")
+        print("uri: \(namespaceURI)")
+        print("qualifiedName: \(qName)")
+        
+    }
+
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        print("char: \(string)")
+    }
+
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        print("end elemet: \(elementName)")
+        print("uri: \(namespaceURI)")
+        print("qualifiedName: \(qName)")
+    }
+
+    func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+        print("\(parseError)")
     }
 
 
--- DpiTransformerTests/DpiTransformerTests.swift
+++ DpiTransformerTests/DpiTransformerTests.swift
@@ -10,27 +10,30 @@ import XCTest
 @testable import DpiTransformer
 
 class DpiTransformerTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        let test = ViewController();
+        test.parseXml("/Users/huibin/text.xml")
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
